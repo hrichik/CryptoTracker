2413afcaaaac4d6e9c4b739e98fd658839e8944e
hrichik <35747890+hrichik@users.noreply.github.com>
2025-09-03 14:35:53 -0400

    Update Program.cs

--- CryptoTracker/CryptoTracker/Program.cs
+++ CryptoTracker/CryptoTracker/Program.cs
@@ -1,46 +1,46 @@
-﻿using System;
+using System;
 using System.Text.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 
-public class Program()
+public class Program() 
 {
 
-    public static async Task<decimal> getCADPrice(decimal price)
+    public static async Task<decimal> getCADPrice(decimal price) //function to take usd value to convert to cad
     {
-        decimal currentPriceCAD = price * (decimal)1.38;
-        return currentPriceCAD;
+        decimal currentPriceCAD = price * (decimal)1.38; //multiplies decimal * decimal in order to find cad
+        return currentPriceCAD; //in the future i will implement an api to get the live conversion rate between cad and usd
                 
     }
     public static async Task getCryptoPrice(string val)
     {
-        string urlAPI = $"https://api.coingecko.com/api/v3/simple/price?ids={val}&vs_currencies=usd";
+        string urlAPI = $"https://api.coingecko.com/api/v3/simple/price?ids={val}&vs_currencies=usd"; //api url
 
-        using HttpClient client = new HttpClient();
+        using HttpClient client = new HttpClient(); //new httpclient is set up
 
         try
         {
-            var response = await client.GetStringAsync(urlAPI);
+            var response = await client.GetStringAsync(urlAPI); //get response based on url and val within url
 
-            using JsonDocument doc = JsonDocument.Parse(response);
-            var root = doc.RootElement;
+            using JsonDocument doc = JsonDocument.Parse(response); //parses the document into C# acceptable structured object
+            var root = doc.RootElement; //gets the root of the json tree, so the outermost {}
 
-            var currentPrice = root.GetProperty(val).GetProperty("usd").GetDecimal();
+            var currentPrice = root.GetProperty(val).GetProperty("usd").GetDecimal(); //
 
             if (currentPrice.Equals(null))
             {
-                Console.WriteLine("No Results Found"); //if none are found, no results found is shown, and this
-                                                       //triggers the else statement in the Main()
+                Console.WriteLine("No Results Found"); //copied this from my other code but this is never actually used here since
+                                                       //errors are handled by the exception
             }
             else
             {
 
-                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Blue; //changes to blue for usd
                 Console.WriteLine($"{val} Price: $" + currentPrice + " USD");
-                decimal cadPrice = await getCADPrice(currentPrice);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{val} Price: $" + Math.Round(cadPrice, 2) + " CAD");
+                decimal cadPrice = await getCADPrice(currentPrice); //await on cadprice to complete
+                Console.ForegroundColor = ConsoleColor.Red; //change to red for cad
+                Console.WriteLine($"{val} Price: $" + Math.Round(cadPrice, 2) + " CAD"); //round to two decimal spots
 
 
             }
@@ -48,10 +48,10 @@ public class Program()
         }
         catch (Exception ex)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Yellow; //yellow for errors
             Console.WriteLine("Error: " + ex);
-            Console.ForegroundColor = ConsoleColor.White;
-
+            Console.ForegroundColor = ConsoleColor.White; //makes sure console returns to white instead of staying yellow
+ 
         }
 
     }
@@ -59,24 +59,26 @@ public class Program()
     public static async Task askForAnotherCrypto()
     {
         await Task.Delay(1000);
-        Console.ForegroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.White; //makes sure its white prior to writing line
         Console.WriteLine("Please enter another cryptocurrency:");
+        //this method asks for another cryptocurrency, i just wanted to explore the functionalities of 
+        //async and await so i made this its own function
     }
 
     public static async Task Main(string[] args)
     {
-        bool running = true;
+        bool running = true; //boolean used to loop program
         
         Console.WriteLine("Hello to the Crypto Tracker!");
         Console.WriteLine("Please enter a cryptocurrency:");
-        while (running)
+        while (running) //while true
         {
-            var userInput = Console.ReadLine();
-            if (userInput == "QQQ")
+            var userInput = Console.ReadLine(); //read user input
+            if (userInput == "QQQ") //if QQQ is inputted the program ends
             {
                 running = false;
             }
-            else
+            else //otherwise, await is called on both the cryptoprice function and askforanothercrypto
             { 
                 await Task.WhenAll(getCryptoPrice(userInput), askForAnotherCrypto());
             }
